6952686df984cb10171c35310857d7d29faa2d0c
Abiram-N <abiram2704@hotmail.com>
2021-05-01 06:09:00 +0530

    main

---
+++ main.c
@@ -0,0 +1,35 @@
+class  A
+{
+	private char* pName;
+	private C* pC;
+
+	public A(){
+	
+		pC = new C();
+		
+	}
+	public char* getName(){
+		
+	}
+	public void setName(char* pName){
+		
+	}
+}
+
+class  B
+{
+	…
+	public B(){
+		
+	}
+	
+}
+
+class  C : public B
+{
+	
+	public C(){
+		
+}
+	
+}
